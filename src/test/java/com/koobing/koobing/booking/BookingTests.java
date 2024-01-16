acd8d498ae66c712b2ad5973728643adf71ee690
Christian Sperandio <christian.sperandio@gmail.com>
2024-01-16 18:44:15 +0100

    add test for post request
    add user mocking

--- src/test/java/com/koobing/koobing/booking/BookingTests.java
+++ src/test/java/com/koobing/koobing/booking/BookingTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -19,6 +20,7 @@ public class BookingTests {
     private MockMvc mvc;
 
     @Test
+    @WithMockUser
     @DisplayName("Book a room")
     void bookRoom() throws Exception {
         var bookingJson = """
