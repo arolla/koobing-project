0e85cd623c3ad80e307a7b442c2cca4bc0c9e9c6
Christian Sperandio <christian.sperandio@gmail.com>
2024-01-18 14:41:48 +0100

    add a fake user details service
    To show the service running for real with roles management.

--- src/main/java/com/koobing/koobing/security/SecurityConfiguration.java
+++ src/main/java/com/koobing/koobing/security/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package com.koobing.koobing.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,5 +31,29 @@ public class SecurityConfiguration {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    @Profile("local")
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user1")
+                .password("password1")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("user2")
+                .password("password2")
+                .roles("USER", "PROCUREMENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    @Profile("local")
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
