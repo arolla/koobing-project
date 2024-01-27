db1120cdc64a362182fb64125b37637c2d80eb29
Christian Sperandio <christian.sperandio@gmail.com>
2024-01-27 14:40:15 +0100

    manage time out call
    (remove useless code)
    Remark about RuntimeException vs. Throwable (and Exception)

--- src/main/java/com/koobing/koobing/search/repository/ResilientSearchRepository.java
+++ src/main/java/com/koobing/koobing/search/repository/ResilientSearchRepository.java
@@ -2,10 +2,7 @@ package com.koobing.koobing.search.repository;
 
 import com.koobing.koobing.search.Hostel;
 import com.koobing.koobing.search.service.HostelRepository;
-import io.github.resilience4j.timelimiter.TimeLimiter;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -16,14 +13,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class ResilientSearchRepository implements HostelRepository {
     private final HostelRepository delegate;
-    private final TimeLimiter timeLimiter;
 
     public ResilientSearchRepository(HostelRepository delegate) {
         this.delegate = delegate;
-        TimeLimiterConfig config = TimeLimiterConfig.custom()
-                .timeoutDuration(Duration.ofMillis(80))
-                .build();
-        this.timeLimiter = TimeLimiter.of("time limiter for hostel search", config);
     }
 
     @Override
